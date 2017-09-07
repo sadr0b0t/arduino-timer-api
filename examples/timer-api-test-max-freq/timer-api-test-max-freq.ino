c1069ca1048788270bde9ab230c12d20c8a20a77
1i7 <ii1i7@yandex.ru>
2017-09-08 00:43:26 +0300

    Замеры производительности для SAM/Arduino Due

--- examples/timer-api-test-max-freq/timer-api-test-max-freq.ino
+++ examples/timer-api-test-max-freq/timer-api-test-max-freq.ino
@@ -20,41 +20,49 @@ void setup() {
 
     // PIC32MX 80MHz (ChipKIT Uno32)
     // AVR 19MHz (Arduino Leonardo)
+    // SAM 84MHz (Arduino Due)
 
     // period = 1us
     // PIC32MX (ChipKIT Uno32): 3us, x2=5us, x10=21us, x1000=2005us (fail)
     // AVR (Arduino Leonardo): 12/16us, x10=80/84/92us (fail)
+    // SAM (Arduino Due): hang, x10=15/16us, x1000=1465us (fail)
     //timer_init_ISR_1MHz(TIMER_DEFAULT);
 
     // period = 2us
     // PIC32MX (ChipKIT Uno32): 4us, x2=6us, x10=22us, x1000=2006us (+2/6us, ~fail)
     // AVR (Arduino Leonardo): 12/16us, x10=84us (fail)
+    // SAM (Arduino Due): hang, x1000=2469us (fail)
     //timer_init_ISR_500KHz(TIMER_DEFAULT);
 
     // period = 5us
     // PIC32MX (ChipKIT Uno32): 5us, x10=50us (ok)
     // AVR (Arduino Leonardo): 12/16us, x2=20us, x10=84us (fail)
+    // SAM (Arduino Due): 5/6us, x2=11us, x10=54/55us, x1000=5460us (fail)
     //timer_init_ISR_200KHz(TIMER_DEFAULT);
 
     // period = 10us
     // PIC32MX (ChipKIT Uno32): 10us (ok)
     // AVR (Arduino Leonardo): 12us, x2=20us, x10=100/104/108us, x1000=10000  (~ok)
-    timer_init_ISR_100KHz(TIMER_DEFAULT);
+    // SAM (Arduino Due): 10/11us, x1000=10397/10425 (?ok)
+    //timer_init_ISR_100KHz(TIMER_DEFAULT);
     
     // period = 20us
     // PIC32MX (ChipKIT Uno32): 20us (ok)
     // AVR (Arduino Leonardo): 12/20/24us, x2=40/44/48us, x1000=19996/20000/20004 (~ok)
+    // SAM (Arduino Due): 20/21us (ok), x1000=20405/20416us (?ok)
     //timer_init_ISR_50KHz(TIMER_DEFAULT);
 
     // period = 50us
     // PIC32MX (ChipKIT Uno32): 50us (ok)
     // AVR (Arduino Leonardo): 48/52/56us, x2=100us (ok)
+    // SAM (Arduino Due): 50/51us, x1000=50410 (?ok)
     //timer_init_ISR_20KHz(TIMER_DEFAULT);
     
     // period = 100us
     // PIC32MX (ChipKIT Uno32): 100us (ok)
     // AVR (Arduino Leonardo): 96/100/104us, x2=200/204us (ok)
-    //timer_init_ISR_10KHz(TIMER_DEFAULT);
+    // SAM (Arduino Due): 100/101us, x1000=100428us (?ok)
+    timer_init_ISR_10KHz(TIMER_DEFAULT);
 }
 
 unsigned long _period;
